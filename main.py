b4d744be6d4972410dacd4a8dc814b924677c4a4
Denis Shvedchenko (dshvedchenko) <dshvedchenko@enova.com>
2023-04-11 17:31:40 +0300

    '''

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import json
+import random
+
+
+def loadR_matrix():
+    with open("rmx.json", "r") as f:
+        R = json.load(f)
+    return R
+
+
+def initQ(R):
+    Q = [[i if i > 0 else 0 for i in line] for line in R]
+    return Q
+
+def all_moves_identical(moves):
+    moves_inv = {v:k for k,v in moves.items}
+
+
+def path_presenter(pth):
+    print(f"= Traverse Path => {pth}")
+
+R = loadR_matrix()
+Q = initQ(R)
+
+
+def findpath(Q, initPos, path_presenter = None):
+    currentPos = initPos
+    learnRate = 0.8
+    stepCounter = 0
+    maxSteps = 1000
+    finalMet = False
+    vertex_traverse = []
+    while not finalMet and stepCounter < maxSteps:
+        vertex_traverse.append(currentPos)
+        stepCounter +=1
+        # get possible move from R[currentPos]
+        possMoves = [(ix,i) for ix,i in enumerate(R[currentPos]) if i > -1]
+        possMoves.sort(key=lambda k: k[1], reverse=True)
+        betterMoveWeight = possMoves[0][1]
+        possMoves = list(filter(lambda k: k[1] == betterMoveWeight, possMoves))
+        # determine random required
+        x = random.randint(0, len(possMoves)-1)
+        nextMovePos, nextMovePrize = possMoves[x]
+        Q[currentPos][nextMovePos] = round(R[currentPos][nextMovePos] + learnRate * max(Q[nextMovePos]))
+        finalMet = currentPos == nextMovePos
+        currentPos = nextMovePos
+    if path_presenter is not None:
+        path_presenter(vertex_traverse)
+    return Q
+
+initPosSet = list(range(len(R)))
+
+while len(initPosSet) > 0:
+    init_ix = random.randint(0, len(initPosSet) - 1)
+    initPos = initPosSet[init_ix]
+    del initPosSet[init_ix]
+    # print(f"Start position: {initPos}")
+    findpath(Q, initPos, path_presenter)
+
+    #
+    for l in Q:
+        print(l)
+    print("-" * 40)
