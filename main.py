0954027e03a8cd40612f5ce7abb788d80ef514fe
Denis Shvedchenko (dshvedchenko) <dshvedchenko@enova.com>
2023-04-18 10:55:56 +0300

    move agent to class

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 
 
 class Agent:
-
     def __init__(self, rmx_file: str = "rmx.txt", path_presenter=None):
         self.rmx_file = rmx_file
         self.R = []
@@ -17,15 +16,14 @@ class Agent:
             n = int(f.readline().strip())
             self.R = [[-1 for i in range(n)] for j in range(n)]
             for line in f.readlines():
-                i,j,w = list(map(lambda x: int(x), line.split(" ")))
+                i, j, w = list(map(lambda x: int(x), line.split(" ")))
                 self.R[i][j] = w
-
 
     def initQ(self):
         self.Q = [[i if i > 0 else 0 for i in line] for line in self.R]
 
     def all_moves_identical(self, moves):
-        moves_inv = {v:k for k,v in moves.items}
+        moves_inv = {v: k for k, v in moves.items}
 
     def findpath(self, initPos):
         currentPos = initPos
@@ -36,16 +34,18 @@ class Agent:
         vertex_traverse = []
         while not finalMet and stepCounter < maxSteps:
             vertex_traverse.append(currentPos)
-            stepCounter +=1
+            stepCounter += 1
             # get possible move from R[currentPos]
-            possMoves = [(ix,i) for ix,i in enumerate(self.R[currentPos]) if i > -1]
+            possMoves = [(ix, i) for ix, i in enumerate(self.R[currentPos]) if i > -1]
             possMoves.sort(key=lambda k: k[1], reverse=True)
             betterMoveWeight = possMoves[0][1]
             possMoves = list(filter(lambda k: k[1] == betterMoveWeight, possMoves))
             # determine random required
-            x = random.randint(0, len(possMoves)-1)
+            x = random.randint(0, len(possMoves) - 1)
             nextMovePos, nextMovePrize = possMoves[x]
-            self.Q[currentPos][nextMovePos] = round(self.R[currentPos][nextMovePos] + learnRate * max(self.Q[nextMovePos]))
+            self.Q[currentPos][nextMovePos] = round(
+                self.R[currentPos][nextMovePos] + learnRate * max(self.Q[nextMovePos])
+            )
             finalMet = currentPos == nextMovePos
             currentPos = nextMovePos
         if path_presenter is not None:
@@ -54,8 +54,10 @@ class Agent:
     def get_possible_nodes(self):
         return list(range(len(self.R)))
 
+
 def path_presenter(pth):
     print(f"=> Traverse Path => {pth}")
+
 
 agent = Agent(path_presenter=path_presenter)
 
